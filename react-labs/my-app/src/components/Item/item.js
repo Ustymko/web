80ae616630fcae4888f2e8c3aa115f9e18e61ccf
Ustym Buchko <ustym.buchko1@gmail.com>
2022-11-07 02:53:25 +0200

    lab 9 code

--- react-labs/my-app/src/components/Item/item.js
+++ react-labs/my-app/src/components/Item/item.js
@@ -2,24 +2,39 @@ import "./item.css"
 import { useLocation } from 'react-router-dom';
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getCar } from "../../API/api";
 
 function Item(){
     const location = useLocation();
     const state = location.state;
-    console.log(state);
+    const [loading, setLoading] = useState(true);
+    const [car, setCar] = useState('');
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+        getCar(state.id,(res) => {
+            setCar(res.data)
+            },(err) => {
+            console.log(err);
+        alert(err);
+        });
+    }, [])
+
     return(
         <>
-            {state && (
+            {loading ? (<div className='loader'></div>) : (
                 <div className="item-page">
                     <div className="item-page__info">
-                        <img alt="car" className="item-page__image" src={state.image}/>
+                        <img alt="car" className="item-page__image" src={car.image_uri}/>
                         <div className="item-page__text">
-                            <h1>{state.producer}</h1>
-                            <p>{state.horsepower}</p>
+                            <h1>{car.producer}</h1>
+                            <h3>{car.horsepower} hp</h3>
                         </div>
                     </div>
                     <div className="item-page__bottom">
-                        <h3>Price: ${state.price}.00</h3>
+                        <h3>Price: ${car.price}.00</h3>
                         <div className="item-page__bottom__buttons">
                             <Button as={Link} to="/catalog" style={{width: 180}} variant="outline-dark">Go back</Button>
                             <Button style={{width: 180}} variant="light">Add to cart</Button>
